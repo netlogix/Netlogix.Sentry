f4ac455022540b6590f3bbb1b40c2400714cf739
Lars Lauger <lars.lauger@netlogix.de>
2021-01-31 00:13:06 +0100

    FEATURE: Initialize sentry after configurationManagerReady Signal
    This way, the ScopeProvider does not have to be instantiated just
    to initialize sentry when no exception is actually thrown. This
    also has the benefit of registering the global error handler
    way earlier in the bootstrap.

---
+++ Classes/Package.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace Netlogix\Sentry;
+
+use Neos\Flow\Configuration\ConfigurationManager;
+use Neos\Flow\Core\Bootstrap;
+use Netlogix\Sentry\Integration\NetlogixIntegration;
+use function Sentry\init;
+
+class Package extends \Neos\Flow\Package\Package
+{
+
+    public function boot(Bootstrap $bootstrap)
+    {
+        $dispatcher = $bootstrap->getSignalSlotDispatcher();
+
+        $dispatcher->connect(ConfigurationManager::class, 'configurationManagerReady', static function(ConfigurationManager $configurationManager) {
+            $dsn = $configurationManager->getConfiguration(ConfigurationManager::CONFIGURATION_TYPE_SETTINGS, 'Netlogix.Sentry.dsn');
+
+            init([
+                'dsn' => $dsn,
+                'integrations' => [
+                    new NetlogixIntegration(),
+                ]
+            ]);
+        });
+    }
+
+}
