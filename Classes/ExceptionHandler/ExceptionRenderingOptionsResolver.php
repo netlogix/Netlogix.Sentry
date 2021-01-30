4bdd91b66d3240c212456217b7ac0777a1857c21
Lars Lauger <lars.lauger@netlogix.de>
2021-01-31 00:09:30 +0100

    FEATURE: Only log exceptions to sentry that have "logException" set to true
    Flow exception rendering groups are taken into account

---
+++ Classes/ExceptionHandler/ExceptionRenderingOptionsResolver.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace Netlogix\Sentry\ExceptionHandler;
+
+use Neos\Flow\Error\AbstractExceptionHandler;
+use Neos\Flow\Annotations as Flow;
+use Throwable;
+
+/**
+ * @Flow\Scope("singleton")
+ * @internal Only used to resolve Exception rendering options. DO NOT USE!
+ */
+final class ExceptionRenderingOptionsResolver extends AbstractExceptionHandler
+{
+
+    public function resolveRenderingOptionsForThrowable(Throwable $throwable): array
+    {
+        return $this->resolveCustomRenderingOptions($throwable);
+    }
+
+    /**
+     * @param Throwable $exception
+     * @internal
+     */
+    public function handleException($exception)
+    {
+        self::throwWhenUsed();
+    }
+
+    protected function echoExceptionWeb($exception)
+    {
+        self::throwWhenUsed();
+    }
+
+    protected function echoExceptionCli(\Throwable $exception)
+    {
+        self::throwWhenUsed();
+    }
+
+    private static function throwWhenUsed(): void
+    {
+        throw new \RuntimeException('This Exception Handler should not be used!', 1612044864);
+    }
+
+}
--- Classes/Integration/NetlogixIntegration.php
+++ Classes/Integration/NetlogixIntegration.php
@@ -4,24 +4,63 @@ declare(strict_types=1);
 namespace Netlogix\Sentry\Integration;
 
 use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Core\Bootstrap;
+use Neos\Flow\ObjectManagement\CompileTimeObjectManager;
+use Netlogix\Sentry\ExceptionHandler\ExceptionRenderingOptionsResolver;
+use Netlogix\Sentry\ScopeProvider;
 use Sentry\Event;
+use Sentry\EventHint;
 use Sentry\Integration\IntegrationInterface;
 use Sentry\SentrySdk;
 use Sentry\State\Scope;
+use Sentry\UserDataBag;
+use Throwable;
 
 /**
  * @Flow\Proxy(false)
+ * @Flow\Autowiring(false)
  */
 final class NetlogixIntegration implements IntegrationInterface
 {
 
     public function setupOnce(): void
     {
-        Scope::addGlobalEventProcessor(static function (Event $event): ?Event {
+        Scope::addGlobalEventProcessor(static function (Event $event, EventHint $hint): ?Event {
+            if (!$hint->exception instanceof Throwable || Bootstrap::$staticObjectManager instanceof CompileTimeObjectManager) {
+                return $event;
+            }
+
             $integration = SentrySdk::getCurrentHub()->getIntegration(self::class);
+            if ($integration === null) {
+                return $event;
+            }
+            assert($integration instanceof NetlogixIntegration);
+
+            $optionsResolver = Bootstrap::$staticObjectManager->get(ExceptionRenderingOptionsResolver::class);
+
+            $options = $optionsResolver->resolveRenderingOptionsForThrowable($hint->exception);
+            if ($options['logException'] ?? false) {
+                return null;
+            }
+
+            self::configureScopeForEvent($event);
 
             return $event;
         });
+    }
+
+    private static function configureScopeForEvent(Event $event): void
+    {
+        try {
+            $scopeProvider = Bootstrap::$staticObjectManager->get(ScopeProvider::class);
+
+            $event->setExtra($scopeProvider->collectExtra());
+            $event->setRelease($scopeProvider->collectRelease());
+            $event->setTags($scopeProvider->collectTags());
+            $userData = $scopeProvider->collectUser();
+            $event->setUser($userData !== [] ? UserDataBag::createFromArray($userData) : null);
+        } catch (\Throwable $t) {
+        }
     }
 
 }
