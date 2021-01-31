c840a8f8ff4929bbab5d3d913a5860c0316e6c4f
Lars Lauger <lars.lauger@netlogix.de>
2021-01-31 11:29:51 +0100

    FEATURE: Make application path of PathPattern configurable

--- Classes/Scope/Release/PathPattern.php
+++ Classes/Scope/Release/PathPattern.php
@@ -12,19 +12,30 @@ final class PathPattern implements ReleaseProvider
 {
 
     /**
-     * @Flow\InjectConfiguration(package="Netlogix.Sentry", path="release.pathPattern")
+     * @var string
+     */
+    protected $pathToMatch;
+
+    /**
      * @var string
      */
     protected $pathPattern;
 
     public function getRelease(): ?string
     {
-        $flowRootPath = trim(realpath(FLOW_PATH_ROOT), '/');
-        if (preg_match($this->pathPattern, $flowRootPath, $matches) === 1) {
+        $path = trim(realpath($this->pathToMatch), '/');
+        if (preg_match($this->pathPattern, $path, $matches) === 1) {
             return $matches[1];
         }
 
         return null;
+    }
+
+    public function injectSettings(array $settings): void
+    {
+        $release = $settings['release'] ?? [];
+        $this->pathToMatch = $release['pathToMatch'] ?? FLOW_PATH_ROOT;
+        $this->pathPattern = $release['pathPattern'] ?? '';
     }
 
 }
